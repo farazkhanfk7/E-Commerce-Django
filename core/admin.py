80635410f6d87a5af761bba11c564872cfbb9a17
farazkhanfk7 <farazkhan138@gmail.com>
2020-06-05 14:18:00 +0530

    fifth commit

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Item, Orderitem, Order
+from .models import Item, Orderitem, Order, Address
 # Register your models here.
 admin.site.register(Item)
 admin.site.register(Orderitem)
 admin.site.register(Order)
+admin.site.register(Address)
--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import ItemDetailView, checkout, HomeView, add_to_cart, add_single_item, remove_from_cart, OrderSummaryView, remove_single_item
+from .views import ItemDetailView, CheckoutView, HomeView, add_to_cart, add_single_item, remove_from_cart, OrderSummaryView, remove_single_item
 
 app_name = 'core'
 
 urlpatterns = [
     path('', HomeView.as_view(), name="home"),
-    path('checkout', checkout, name="checkout"),
+    path('checkout', CheckoutView.as_view(), name="checkout"),
     path('order-summary', OrderSummaryView.as_view(), name="order-summary"),
     path('product/<slug>/', ItemDetailView.as_view(), name="product"),
     path('add-to-cart/<slug>', add_to_cart, name="add_to_cart"),
